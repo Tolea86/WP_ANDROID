cc7a7bebb8ba0abb950a092710a9a6e090ce9af1
Tolea <iuzvac.anatolie@gmail.com>
2017-03-27 00:05:54 +0300

    Added text to the window

--- LAB_1/PW_lab1/app/src/main/java/com/tolea/pw_lab1/MainActivity.java
+++ LAB_1/PW_lab1/app/src/main/java/com/tolea/pw_lab1/MainActivity.java
@@ -2,14 +2,37 @@ package com.tolea.pw_lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout mainView;
+
+    TextView textCenter;
+
+    String middleText = "Done with Pride and Prejudice by Iuzvac Anatolie";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainView = (RelativeLayout) findViewById(R.id.activityMainView);
 
+        createMiddleText();
+    }
+
+    private void createMiddleText(){
+        textCenter = new TextView(this);
+
+        RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams
+                ((int)RelativeLayout.LayoutParams.WRAP_CONTENT,(int) RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        textCenter.setText(middleText);
+        textCenter.setTextSize((float) 14);
+        textCenter.setLayoutParams(params);
+
+        mainView.addView(textCenter);
     }
 }
