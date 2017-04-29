9bb0ac04030f9a1062ab412495a7403504c384ae
Tolea <iuzvac.anatolie@gmail.com>
2017-04-30 01:02:26 +0300

    Hooked 2 keyboard inputs with 2 different actions.

--- LAB_2/PW_LAB2/app/src/main/java/com/tolea/pw_lab2/MainActivity.java
+++ LAB_2/PW_LAB2/app/src/main/java/com/tolea/pw_lab2/MainActivity.java
@@ -1,27 +1,35 @@
 package com.tolea.pw_lab2;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import java.util.Random;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout mainView;
 
     Button centerButton;
+    Button hookKeyboardInputButton;
 
     Toolbar menuBar;
+
+    ProgressDialog dialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         mainView = (RelativeLayout) findViewById(R.id.mainView);
 
         centerButton = (Button) findViewById(R.id.centerButton);
+        hookKeyboardInputButton = (Button) findViewById(R.id.hookKeyboardInputButton);
 
         centerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +58,18 @@ public class MainActivity extends AppCompatActivity {
                                 });
                 AlertDialog alert = builder.create();
                 alert.show();
+            }
+        });
+
+
+        InputMethodManager inputMethodManager=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+
+        hookKeyboardInputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputMethodManager inputMethodManager=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
             }
         });
     }
@@ -99,5 +120,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch(keyCode){
+            case KeyEvent.KEYCODE_S:
 
+                dialog = ProgressDialog.show(this, "", "S PRESSED");
+
+                new java.util.Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.dismiss();
+                            }
+                        });
+                    }
+                }, 1000);
+
+                break;
+            case KeyEvent.KEYCODE_SPACE:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this);
+                builder.setMessage("Hey!")
+                        .setTitle("Super Secret Key Pressed")
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+                break;
+        }
+
+        return true;
+    }
 }
