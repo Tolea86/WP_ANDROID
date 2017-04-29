0454c334165db01169a97c2ae980905dfcfda00c
Tolea <iuzvac.anatolie@gmail.com>
2017-04-30 00:40:07 +0300

    Added a system menu to applciation with 3 items with actions to it.

--- LAB_2/PW_LAB2/app/src/main/java/com/tolea/pw_lab2/MainActivity.java
+++ LAB_2/PW_LAB2/app/src/main/java/com/tolea/pw_lab2/MainActivity.java
@@ -2,17 +2,26 @@ package com.tolea.pw_lab2;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout mainView;
 
     Button centerButton;
+
+    Toolbar menuBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +52,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu_bar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.showAMessage:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this);
+                builder.setMessage("Hey!")
+                        .setTitle("Super Dialog Box")
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+                break;
+            case R.id.deleteCenterButton:
+
+                centerButton.setVisibility(View.GONE);
+
+                break;
+            case R.id.changeBackgroundColor:
+
+
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                mainView.setBackgroundColor(color);
+
+                break;
+        }
+        return true;
+    }
+
 
 
 }
