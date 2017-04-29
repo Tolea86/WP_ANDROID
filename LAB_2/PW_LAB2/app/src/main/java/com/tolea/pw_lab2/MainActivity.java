0bbefc117fc8f974d4d245f7605cf880aa93d72b
Tolea <iuzvac.anatolie@gmail.com>
2017-04-30 00:14:31 +0300

    Added button in center and dialog to appear on clicking

--- LAB_2/PW_LAB2/app/src/main/java/com/tolea/pw_lab2/MainActivity.java
+++ LAB_2/PW_LAB2/app/src/main/java/com/tolea/pw_lab2/MainActivity.java
@@ -1,13 +1,48 @@
 package com.tolea.pw_lab2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout mainView;
+
+    Button centerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainView = (RelativeLayout) findViewById(R.id.mainView);
+
+        centerButton = (Button) findViewById(R.id.centerButton);
+
+        centerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(
+                        MainActivity.this);
+                builder.setMessage("Hey!")
+                        .setTitle("Super Dialog Box")
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
     }
+
+
 }
